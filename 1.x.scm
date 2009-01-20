80692f5b7c3c732d9a4f6fcb599c3e9014fdf7b3
mariorz <mariorz@gmail.com>
2009-01-20 01:02:06 -0600

    added example

--- 1.x.scm
+++ 1.x.scm
@@ -7,9 +7,9 @@
 
 (define (sum-of-2sq a b c)
 	(cond
-	[(and (< a b) (< a c)) (+ (sq b) (sq c))]		
-	[(and (< b a) (< b c)) (+ (sq a) (sq c))]
-	[(and (< c a) (< c b)) (+ (sq a) (sq b))]))
+	[(and (<= a b) (<= a c)) (+ (sq b) (sq c))]		
+	[(and (<= b a) (<= b c)) (+ (sq a) (sq c))]
+	[(and (<= c a) (<= c b)) (+ (sq a) (sq b))]))
 
 
 
@@ -21,7 +21,7 @@
 (define (a-plus-abs-b a b) 
   ((if (> b 0) + -) a b)) 
 
-
+;; if b is positive adds it to a, otherwise substract it (a plus absolute b)
 
 
 
@@ -65,6 +65,41 @@
 ;;          (sqrt-iter (improve guess x) 
 ;;                     x)))
 ;;What happens when Alyssa attempts to use this to compute square roots? Explain. 
+
+
+
+
+
+
+;;Now let's formalize the process in terms of procedures. We start with a value for the radicand (the number 
+;;whose square root we are trying to compute) and a value for the guess. If the guess is good enough for our 
+;;purposes, we are done; if not, we must repeat the process with an improved guess. We write this basic 
+;;strategy as a procedure: 
+(define (sqrt-iter guess x) 
+  (if (good-enough? guess x) 
+      guess 
+      (sqrt-iter (improve guess x) 
+                 x))) 
+
+;;A guess is improved by averaging it with the quotient of the radicand and the old guess: 
+(define (improve guess x) 
+  (average guess (/ x guess))) 
+
+;;where 
+
+(define (average x y) 
+  (/ (+ x y) 2)) 
+
+;;We also have to say what we mean by ``good enough.'' The following will do for illustration, but it is not 
+;;really a very good test. (See exercise 1.7.) The idea is to improve the answer until it is close enough so that 
+;;its square differs from the radicand by less than a predetermined tolerance (here 0.001):22 
+(define (good-enough? guess x) 
+  (< (abs (- (square guess) x)) 0.001)) 
+
+;;Finally, we need a way to get started. For instance, we can always guess that the square root of any 
+;;number is 1:23
+(define (sqrt x) 
+  (sqrt-iter 1.0 x)) 
 
 
 
