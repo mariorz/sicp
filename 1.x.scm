8c12b5868586346d46c5ea1c398dd64555def60d
mariorz <mariorz@gmail.com>
2009-01-22 22:49:29 -0600

    all answered

--- 1.x.scm
+++ 1.x.scm
@@ -16,9 +16,8 @@
 
 
   
-
-
-;;Exercise 1.4.  Observe that our model of evaluation allows for combinations whose operators are 
+;;Exercise 1.4. 
+;;Observe that our model of evaluation allows for combinations whose operators are 
 ;;compound expressions. Use this observation to describe the behavior of the following procedure: 
 (define (a-plus-abs-b a b) 
   ((if (> b 0) + -) a b)) 
@@ -28,7 +27,9 @@
 
 
 
-;;Exercise 1.5.  Ben Bitdiddle has invented a test to determine whether the interpreter he is faced with is 
+
+;;Exercise 1.5.  
+;;Ben Bitdiddle has invented a test to determine whether the interpreter he is faced with is 
 ;;using applicative-order evaluation or normal-order evaluation. He defines the following two procedures: 
 ;;(define (p) (p)) 
 ;;(define (test x y) 
@@ -43,13 +44,14 @@
 ;;applicative order: The predicate expression is evaluated first, and the result determines whether to evaluate 
 ;;the consequent or the alternative expression.) 
 
+;; applicative order loops on itself as the (p) operand never stops beeing re-evaluated
+;; normal order runs into an infinite loop as it never stops expanding,
 
 
 
 
-
-
-;;Exercise 1.6.  Alyssa P. Hacker doesn't see why if needs to be provided as a special form. ``Why can't I 
+;;Exercise 1.6.  
+;;Alyssa P. Hacker doesn't see why if needs to be provided as a special form. ``Why can't I 
 ;;just define it as an ordinary procedure in terms of cond?'' she asks. Alyssa's friend Eva Lu Ator claims 
 ;;this can indeed be done, and she defines a new version of if: 
 ;;(define (new-if predicate then-clause else-clause) 
@@ -69,56 +71,18 @@
 ;;What happens when Alyssa attempts to use this to compute square roots? Explain. 
 
 
-
-
-
-
-;;Now let's formalize the process in terms of procedures. We start with a value for the radicand (the number 
-;;whose square root we are trying to compute) and a value for the guess. If the guess is good enough for our 
-;;purposes, we are done; if not, we must repeat the process with an improved guess. We write this basic 
-;;strategy as a procedure: 
-(define (sqrt-iter guess x) 
-  (if (good-enough? guess x) 
-      guess 
-      (sqrt-iter (improve guess x) 
-                 x))) 
-
-
-(define (sqrt-iter2 guess x) 
-  (if (good-enough2? guess x) 
-      guess 
-      (sqrt-iter2 (improve guess x) 
-                 x))) 
-
-;;A guess is improved by averaging it with the quotient of the radicand and the old guess: 
-(define (improve guess x) 
-  (average guess (/ x guess))) 
-
-;;where 
-
-(define (average x y) 
-  (/ (+ x y) 2)) 
-
-;;We also have to say what we mean by ``good enough.'' The following will do for illustration, but it is not 
-;;really a very good test. (See exercise 1.7.) The idea is to improve the answer until it is close enough so that 
-;;its square differs from the radicand by less than a predetermined tolerance (here 0.001):22 
-
-;; original version
-(define (good-enough2? guess x) 
-  (< (abs (- (square guess) x)) .001)) 
-
-
-
-;;Finally, we need a way to get started. For instance, we can always guess that the square root of any 
-;;number is 1:23
-(define (sqrt x) 
-  (sqrt-iter 1.0 x)) 
+;; infinite loops! the interpreter uses applicative-order evaluation, which causes the function to recur
+;; on itself even when the predicate evaluates to false. lesson learned: if needs to be a special form so that
+;; the else-clause doesn´t get evaluated by aplicative-order.
 
 
 
 
 
-;;Exercise 1.7.  The good-enough? test used in computing square roots will not be very effective for 
+
+
+;;Exercise 1.7.  
+;;The good-enough? test used in computing square roots will not be very effective for 
 ;;finding the square roots of very small numbers. Also, in real computers, arithmetic operations are almost 
 ;;always performed with limited precision. This makes our test inadequate for very large numbers. Explain 
 ;;these statements, with examples showing how the test fails for small and large numbers. An alternative 
@@ -126,16 +90,72 @@
 ;;next and to stop when the change is a very small fraction of the guess. Design a square-root procedure that 
 ;;uses this kind of end test. Does this work better for small and large numbers? 
 
+(define (square x) (* x x))
 
-;; My version just modifies good-enough?
-(define (good-enough? guess x) 
-  (< (abs (- (square guess) x)) (abs (/ x 1000000000)))) 
-
-
+(define (improve guess x) 
+  (average guess (/ x guess))) 
 
 
+(define (average x y) 
+  (/ (+ x y) 2)) 
 
-;;Exercise 1.8.  Newton's method for cube roots is based on the fact that if y is an approximation to the cube 
+;; original version
+
+(define (orig-sqrt-iter guess x) 
+  (if (orig-good-enough? guess x) 
+      guess 
+      (orig-sqrt-iter (improve guess x) 
+                 x))) 
+
+(define (orig-good-enough? guess x) 
+  (< (abs (- (square guess) x)) .001)) 
+
+(define (orig-sqrt x) 
+  (orig-sqrt-iter 1.0 x)) 
+
+;; alternate version 
+
+(define (my-sqrt-iter guess x) 
+  (if (my-good-enough? guess x) 
+      guess 
+      (my-sqrt-iter (improve guess x) 
+                 x))) 
+
+(define (my-good-enough? guess x) 
+  (< (abs (- (square guess) x)) (abs (* x .0000000001)))) 
+
+(define (my-sqrt x) 
+  (my-sqrt-iter 1.0 x)) 
+
+
+;; assigned version
+
+
+(define (sqrt-iter guess old-guess x) 
+  (if (good-enough? guess old-guess x) 
+      guess 
+      (sqrt-iter (improve guess x)
+                 guess x))) 
+
+(define (improve guess x) 
+  (average guess (/ x guess))) 
+
+
+(define (average x y) 
+  (/ (+ x y) 2)) 
+
+(define (good-enough? guess old-guess x) 
+  (< (abs (- guess old-guess)) (abs (* guess 0.0000000001)))) 
+
+
+(define (sqrt x) 
+  (sqrt-iter 1.0 x x)) 
+
+
+
+
+;;Exercise 1.8.  
+;;Newton's method for cube roots is based on the fact that if y is an approximation to the cube 
 ;;root of x, then a better approximation is given by the value 
 ;;
 ;;(/ (+ (/ x (sq y)) (* y 2)) 3)
@@ -144,25 +164,21 @@
 ;;section 1.3.4 we will see how to implement Newton's method in general as an abstraction of these square- 
 ;;root and cube-root procedures.) 
 
+(define (sq x) (* x x))
 
-(define (sqrt-iter guess x) 
-  (if (good-enough? guess x) 
+(define (cube x) (* x x x))
+
+(define (cube-iter guess x) 
+  (if (cube-good-enough? guess x) 
       guess 
-      (sqrt-iter (improve guess x) 
+      (cube-iter (cube-improve guess x) 
                  x))) 
 
-(define (improve guess x) 
-  (average guess (/ x guess))) 
+(define (cube-improve guess x)
+  (/ (+ (/ x (sq guess)) (* guess 2)) 3))
+  
+(define (cube-good-enough? guess x) 
+  (< (abs (- (cube guess) x)) (abs (* x .0000000001)))) 
 
-
-(define (average x y) 
-  (/ (+ x y) 2)) 
-
-
-
-(define (good-enough? guess x) 
-  (< (abs (- (square guess) x)) (abs (/ x 1000000000)))) 
-
-
-(define (sqrt x) 
-  (sqrt-iter 1.0 x)) 
+(define (cubert x) 
+  (cube-iter 1.0 x)) 
